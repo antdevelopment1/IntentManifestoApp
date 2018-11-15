aa5c3259a7d33c4b610dc8d3bac773626e7b12b9
April Copes <antdevelopment1@gmail.com>
2018-11-14 21:29:21 -0500

    Adds database tables for buJoApp

---
+++ schema.sql
@@ -0,0 +1,18 @@
+-- Bullet Journal App
+
+-- Users Table
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    username VARCHAR(200) UNIQUE NOT NULL,
+    pwhash VARCHAR(60) NOT NULL
+);
+
+-- Entries Table
+CREATE TABLE entries (
+    id SERIAL PRIMARY KEY,
+    title TEXT,
+    entry TEXT,
+    date TIMESTAMP,
+    user_id INTEGER REFERENCES users (id) ON DELETE CASCADE
+);
