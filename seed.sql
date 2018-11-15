6e3e204096aa88629e6f1adcc0f819779d7cc35a
April Copes <antdevelopment1@gmail.com>
2018-11-14 21:51:55 -0500

    Adds entries table to database

--- seed.sql
+++ seed.sql
@@ -8,3 +8,13 @@ INSERT INTO users (name, username, pwhash) VALUES('Jackie', 'jaidragon', '$2b$10
 INSERT INTO users (name, username, pwhash) VALUES('Jessy', 'jaydbee', '$2b$10$QUJ346ylCYC7rIS2cbqEq.l4o.HCL6r.pZokcF6Tivha/UTXDi.JK');
 INSERT INTO users (name, username, pwhash) VALUES('Chris', 'peapod', '$2b$10$QUJ346ylCYC7rIS2cbqEq.l4o.HCL6r.pZokcF6Tivha/UTXDi.JK');
 
+-- Dummy Data For Entries Table
+INSERT INTO entries (title, date, entry, user_id) VALUES ('I love watercolors.', '2018-09-12', 'I know just where the paintbrush is. ...', 1);
+INSERT INTO entries (title, date, entry, user_id) VALUES ('I love ice cream.', '2017-09-09', 'Oh oreo. You own my heart. Yes in did. Burr!!!...', 2);
+INSERT INTO entries (title, date, entry, user_id) VALUES ('I love music.', '2018-02-03', 'I wanna rock and roll all night and lick some cocaine...', 3);
+INSERT INTO entries (title, date, entry, user_id) VALUES ('I love graphic design.', '2018-06-11', 'I like to make cool circles and triangles...', 4);
+INSERT INTO entries (title, date, entry, user_id) VALUES ('I love my kitties.', '2018-09-13', 'I like to make Moo and Cy chase lasers......', 5);
+INSERT INTO entries (title, date, entry, user_id) VALUES ('I love tool and drums.', '2016-01-05', 'Bang Bang Bang Cymbal Hihat Bang Polyrythms. Sacred Geometry.', 6);
+
+
+
