e776ac3e43a3b93d726e9262ef21345c7cae4e89
April Copes <antdevelopment1@gmail.com>
2018-11-14 23:17:50 -0500

    Creates class for uUser

---
+++ models/users.js
@@ -0,0 +1,29 @@
+const db = require('./db');
+
+class User {
+    constructor(id, name, username, pwhash) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.pwhash = pwhash;
+    }
+
+
+//     // Create
+    
+
+
+//     // Retrieve
+
+        
+//     // Update
+
+
+
+
+//     // Delete
+}
+
+module.exports = User;
+
+
