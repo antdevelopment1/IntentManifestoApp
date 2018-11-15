b5f95ab843a575ab5b60d2cb46f724c0efa57587
April Copes <antdevelopment1@gmail.com>
2018-11-14 21:36:52 -0500

    Adds users table dummy data

---
+++ seed.sql
@@ -0,0 +1,10 @@
+-- Bullet Journal App
+
+-- Dummy Data For Users Table
+INSERT INTO users (name, username, pwhash) VALUES('Lauren', 'lilylove', '$2b$10$QUJ346ylCYC7rIS2cbqEq.l4o.HCL6r.pZokcF6Tivha/UTXDi.JK');
+INSERT INTO users (name, username, pwhash) VALUES('April', 'doodle', '$2b$10$QUJ346ylCYC7rIS2cbqEq.l4o.HCL6r.pZokcF6Tivha/UTXDi.JK');
+INSERT INTO users (name, username, pwhash) VALUES('Chris', 'cloud', '$2b$10$QUJ346ylCYC7rIS2cbqEq.l4o.HCL6r.pZokcF6Tivha/UTXDi.JK');
+INSERT INTO users (name, username, pwhash) VALUES('Jackie', 'jaidragon', '$2b$10$QUJ346ylCYC7rIS2cbqEq.l4o.HCL6r.pZokcF6Tivha/UTXDi.JK');
+INSERT INTO users (name, username, pwhash) VALUES('Jessy', 'jaydbee', '$2b$10$QUJ346ylCYC7rIS2cbqEq.l4o.HCL6r.pZokcF6Tivha/UTXDi.JK');
+INSERT INTO users (name, username, pwhash) VALUES('Chris', 'peapod', '$2b$10$QUJ346ylCYC7rIS2cbqEq.l4o.HCL6r.pZokcF6Tivha/UTXDi.JK');
+
