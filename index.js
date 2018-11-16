b6cfc18c661a620a516e43348e7dac9e73c11be5
April Copes <antdevelopment1@gmail.com>
2018-11-15 20:10:51 -0500

    Gets user by ID

--- index.js
+++ index.js
@@ -5,11 +5,28 @@ const User = require('./models/users');
 // // const app = express();
 
 
-User.getAll()
-    .then( all => { console.log(all); })
+// Get all users
+// User.getAll()
+//     .then( all => { console.log(all); })
+
+// Gets user by id
+User.getById(5)
+
+    
 
 
+// ==============================
+// Random chaining example
+// ==============================
+// User.addUser('Sam', '678')
+    // .then(santa => {
+    //     // console.log(santa);
+    //     santa.getPassword()
+    //         .then(password => {
+    //             console.log(password.phash);
+    //         })
 
+    // })
 
 
 
