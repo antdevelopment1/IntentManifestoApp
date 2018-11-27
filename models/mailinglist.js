ad80bdf98496045c0347e42563157337984ea965
April Copes <antdevelopment1@gmail.com>
2018-11-27 09:48:36 -0500

    Adds new file for email class

---
+++ models/mailinglist.js
@@ -0,0 +1,33 @@
+const db = require('./db');
+
+// Class for new subscriber but not nessisarily a member
+class Email {
+    constructor(id, name, email) {
+
+    }
+
+}
+
+
+
+
+
+// ======================
+//     Create Email
+// ======================
+
+
+// ======================
+//    Retrieve Email
+// ======================
+
+// ======================
+//     Update Email
+// ======================
+
+
+// ======================
+//     Delete Email
+// ======================
+
+module.exports = Email;
